3e2937202e85d7fddbb1dff42ac6d33005260f87
Md Imran Hossain <imranhossain1402@gmail.com>
2023-12-04 15:20:41 +0100

    User retireve and update done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { AdminRoutes } from '../modules/admin/admin.route';
+import { UserRoutes } from '../modules/user/user.route';
 
 const router = express.Router();
 
@@ -7,7 +8,10 @@ const moduleRoutes = [
   {
     path: '/admin',
     route: AdminRoutes,
-  }
+  },  {
+    path: '/user',
+    route: UserRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
